cf37a0388b452faada21ecdcc348833b58e8c9c0
Melissa Marshall <mveritym@gmail.com>
2015-07-03 22:23:31 -0700

    Figuring out margins

--- static/js/data.js
+++ static/js/data.js
@@ -1,31 +1,38 @@
-var maxHeight = 500, maxWidth = 700, textBufferWidth = 5;
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    axisWidth = 40,
+    maxWidth = 960 - margin.left - margin.right,
+    maxHeight = 500 - margin.top - margin.bottom,
+    textBufferWidth = 5;
 
 var scale = d3.scale.linear()
     .range([0, maxHeight]);
 
-var chart = d3.select(".chart")
+var yScale = d3.scale.linear()
+    .range([0, maxHeight]);
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left")
+    .ticks(20);
+
+var chart = d3.select(".chart");
 
 d3.csv('data/genders_by_role.csv', function(error, data) {
     scale.domain([0, d3.max(data, function(d) { return get_total(d); })]);
-    //
-    // data.forEach(function(d) {
-    //     console.log(d["Num.Men"] + " " + d["Num.Women"] + " " + get_total(d));
-    //     console.log(scale(d["Num.Men"]) + " " + scale(d["Num.Women"]) + " " + scale(get_total(d)));
-    //     console.log("");
-    // });
+    yScale.domain([d3.max(data, function(d) { return get_total(d); }), 0]);
 
     var barWidth = maxWidth / data.length;
 
     var bar = chart.selectAll("g")
         .data(data)
         .enter().append("g")
-        .attr("transform", function(d, index) { return "translate(" + index * barWidth + ",0)"; });
+        .attr("transform", function(d, index) { console.log((index*barWidth) + axisWidth); return "translate(" + ((index * barWidth) + axisWidth) + ",0)"; });
 
     bar.append("text")
         .text(function(d) { return d["Role"]; })
         .attr("x", function(d) { return - maxHeight - textBufferWidth; })
         .attr("transform", "rotate(270)")
-        .attr("dy", "1.35em");
+        .attr("dy", "1.75em");
 
     bar.append("rect")
         .attr("y", function(d) { return maxHeight - scale(get_total(d)) })
@@ -34,15 +41,19 @@ d3.csv('data/genders_by_role.csv', function(error, data) {
         .attr("class", "women");
 
     bar.append("rect")
-    // scale(get_total(d)) - d["Num.Women"]
         .attr("y", function(d) { return maxHeight - scale(d["Num.Men"]); })
         .attr("width", barWidth - 1)
         .attr("height", function(d) { return scale(d["Num.Men"]); })
         .attr("class", "men");
 
+    chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + (axisWidth-5) + ",0)")
+        .call(yAxis);
+
     chart
         .attr("height", maxHeight + getMaxTextLength() + textBufferWidth)
-        .attr("width", maxWidth);
+        .attr("width", maxWidth + axisWidth);
 });
 
 function get_total(d) {
