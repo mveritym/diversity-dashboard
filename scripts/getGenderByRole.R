4105ea1e820251a5926b6cab87a8cf09ff65b6a8
Melissa Marshall <mveritym@gmail.com>
2015-07-05 22:52:04 -0700

    Updating R script to pass back outfile name and check for existing directory

--- scripts/getGenderByRole.R
+++ scripts/getGenderByRole.R
@@ -2,13 +2,22 @@ library(dplyr)
 
 tech_roles <- c("Dev", "UI Dev", "QA", "DevOps", "Infra Cons", "Net Admin", "Tech Admin", "Info Sec")
 
-consultants_raw <- read.csv("inputs/consultant_info.csv")
+subdir <- "data/generated"
+generated_dir <- paste(getwd(), subdir, sep="/")
+if (!file.exists(generated_dir)){
+    dir.create(file.path(generated_dir))
+}
+
+consultants_raw <- read.csv("data/input/consultant_info.csv")
 consultants <- select(consultants_raw, -(Name), -(Home.Office:X), -(Available.From:Planned.Assignment))
 consultants <- filter(consultants, Gender != "Unknown")
 
-gender_by_role <- summarise(group_by(consultants, Role), num_men = sum(Gender == "Male"), num_women = sum(Gender == "Female"))
-gender_by_role <- mutate(gender_by_role, Technical = Role %in% tech_roles)
+gender_by_role <- summarise(group_by(consultants, Role),
+                            num_men = as.character(sum(Gender == "Male")),
+                            num_women = as.character(sum(Gender == "Female")))
+gender_by_role <- mutate(gender_by_role, Technical = as.character(Role %in% tech_roles))
 
-write.csv(gender_by_role, file="static/data/gender_by_role.csv", row.names=FALSE)
+outfile <- paste(subdir, "gender_by_role.csv", sep="/")
+write.table(gender_by_role, file=outfile, row.names=FALSE)
 
-print("DONE WITH R SCRIPT!")
+print(outfile)
