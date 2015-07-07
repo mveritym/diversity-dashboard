6771db5e120b6db72929d6919a38c8d09cb5c0c8
Melissa Marshall <mveritym@gmail.com>
2015-07-06 19:22:36 -0700

    Adding view controller thing

---
+++ static/js/controlViews.js
@@ -0,0 +1,20 @@
+$(function() {
+    show_file_uploader();
+    hide_chart();
+})
+
+function show_file_uploader () {
+    $("#include-dropzone").show();
+}
+
+function hide_file_uploader () {
+    $("#include-dropzone").hide();
+}
+
+function show_chart () {
+    $("#include-chart").show();
+}
+
+function hide_chart () {
+    $("#include-chart").hide();
+}
